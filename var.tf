9d180e8994536dd3c11f467c0a20f229ca0ae274
SivadeviAluri <sivadevialuri85@gmail.com>
2026-03-05 10:51:29 +0530

    Add Terraform variables for project configuration

---
+++ var.tf
@@ -0,0 +1,13 @@
+variable "project_id" {
+    type = string
+    default = devops-489105
+}
+variable "region" { 
+    type = string
+    default = "us-central1"
+}
+
+variable "zone"{
+    type = string
+    default = "us-central1-a"
+}
